69f330b34370cf88deb49cd0e1622030dc8d8cff
barissamur <barissamurr@gmail.com>
2023-03-19 23:24:49 +0300

    son hali

--- HamburgerProje/Controllers/AdminController.cs
+++ HamburgerProje/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using HamburgerProje.Data;
 using HamburgerProje.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using System;
 
 namespace HamburgerProje.Controllers
 {
+    [Authorize(Roles ="admin")]
     public class AdminController : Controller
     {
         private readonly UygulamaDbContext _db;
